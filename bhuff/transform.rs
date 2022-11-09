2293326a589b2f4b902f7318324360382f784d69
Marc Schoolderman <info@squell.net>
2022-11-10 00:35:57 +0100

    bug: BWT requires stable sorting (nice one)

--- bhuff/transform.rs
+++ bhuff/transform.rs
@@ -4,7 +4,7 @@
 
 fn bw_transform<T: Ord+Copy>(data: &Vec<T>) -> Vec<Option<T>> {
     let mut range: Vec<usize> = (0..=data.len()).collect();
-    range.sort_unstable_by_key(|&i| &data[i..]);
+    range.sort_by_key(|&i| &data[i..]);
     return range.iter().map(|&i| if i > 0 { Some(data[i-1]) } else { None } ).collect();
 }
 
@@ -13,7 +13,7 @@ fn bw_reverse<T: Ord+Copy>(data: &Vec<Option<T>>) -> Vec<T> {
     let mut range: Vec<(Option<T>,usize)> = data.iter().cloned().zip(0..).collect();
     let mut out = Vec::new();
     out.reserve(range.len()-1);
-    range.sort_unstable_by_key(|&(c,_)| c);
+    range.sort_by_key(|&(c,_)| c);
     let mut i = range[0].1;
     for _ in 1..range.len() {
         out.push(range[i].0.unwrap());
